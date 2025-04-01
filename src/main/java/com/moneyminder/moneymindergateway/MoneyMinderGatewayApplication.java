24e25fcc5ab775d1e7db46ccfc7efe10cef61825
saul <smartinezpad@uoc.edu>
2025-04-01 19:01:30 +0200

    Initial commit

---
+++ src/main/java/com/moneyminder/moneymindergateway/MoneyMinderGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.moneyminder.moneymindergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMinderGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyMinderGatewayApplication.class, args);
+    }
+
+}
