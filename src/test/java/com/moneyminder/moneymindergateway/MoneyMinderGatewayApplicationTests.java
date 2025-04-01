81f736c13b0abed583ea43a82aa75091ccb57daf
saul <smartinezpad@uoc.edu>
2025-04-01 19:01:30 +0200

    Initial commit

---
+++ src/test/java/com/moneyminder/moneymindergateway/MoneyMinderGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneyminder.moneymindergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMinderGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
