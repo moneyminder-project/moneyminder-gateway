bac3b1ce4d11882a7fe21678c0478aa079d43e2d
saul <smartinezpad@uoc.edu>
2025-05-07 17:21:49 +0200

    Changes because created DockerFile

--- src/main/java/com/moneyminder/moneymindergateway/security/SecurityConfig.java
+++ src/main/java/com/moneyminder/moneymindergateway/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // ⚠️ Ajusta el origen
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8080"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
